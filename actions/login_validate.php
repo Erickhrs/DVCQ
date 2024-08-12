0cceaa6e158817979586767a02f3fc3893bd4db8
Erickhrs <erickhenriquee4444@gmail.com>
2024-08-12 12:13:25 -0300

    login system

---
+++ actions/login_validate.php
@@ -0,0 +1,88 @@
+<?php
+include('../includes/connection.php');
+session_start();
+if(isset($_POST['email'], $_POST['password'])) {
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $password = $_POST['password'];
+
+    // Busca o usuário pelo email
+    $sql_code = "SELECT * FROM users WHERE email = '$email' AND status = 1";
+    $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+    if($sql_query->num_rows == 1) {
+        $usuario = $sql_query->fetch_assoc();
+        $hashedPassword = $usuario['psw'];
+
+        // Verifica se a senha está correta
+        if(password_verify($password, $hashedPassword)) {
+            session_regenerate_id();
+            // Define as variáveis de sessão
+            $_SESSION['id'] = $usuario['ID'];
+            $_SESSION['name'] = $usuario['name'];
+
+            header("Location: ../index.php");
+            exit;
+        } else {
+            //echo "Senha incorreta";
+            $_SESSION['erro'] = "Senha incorreta";
+            header("Location: ../login.php");
+        }
+    } else {
+        //echo "Usuário não encontrado";
+         $_SESSION['erro'] = "Usuário não encontrado";
+        header("Location: ../login.php");
+    }
+} else {
+    //echo "Preencha todos os campos";
+    $_SESSION['erro'] = "Preencha todos os campos";
+    header("Location: ../login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loading Screen</title>
+    <style>
+    body,
+    html {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #1d3969;
+        color: white;
+        font-family: Arial, sans-serif;
+    }
+
+    .loader {
+        border: 16px solid #f3f3f3;
+        border-top: 16px solid #3498db;
+        border-radius: 50%;
+        width: 120px;
+        height: 120px;
+        animation: spin 2s linear infinite;
+    }
+
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    </style>
+</head>
+
+<body>
+    <div class="loader"></div>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<!-- Coding by CodingLab | www.codinglabweb.com-->
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DVC - QUESTÕES </title>
+
+    <!-- CSS -->
+    <link rel="shortcut icon" href="./assets/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./style/login.css">
+    <link rel="stylesheet" href="./style/global.css">
+
+    <!-- Boxicons CSS -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+
+<body>
+    <section class="container forms">
+        <div class="form login">
+            <div class="form-content">
+                <header>Seja bem-vindo de volta! :)</header>
+
+                <form action="./actions/login_validate.php" method="post">
+                    <?php
+                    if(isset($_SESSION['erro'])){
+                        echo '<span class="erroMSG">'.$_SESSION['erro'].'</span>';
+                    } ?>
+                    <div class="field input-field">
+                        <input type="email" placeholder="Email" class="input" name="email">
+                    </div>
+
+                    <div class="field input-field">
+                        <input type="password" placeholder="Password" class="password" name="password">
+                        <i class='bx bx-hide eye-icon'></i>
+                    </div>
+
+                    <div class="form-link">
+                        <a href="#" class="forgot-pass">Forgot password?</a>
+                    </div>
+
+                    <div class="field button-field">
+                        <button type="submit">Entrar</button>
+                    </div>
+                </form>
+
+                <div class="form-link">
+                    <span>Don't have an account? <a href="./signup.html" class="link signup-link">Signup</a></span>
+                </div>
+            </div>
+
+            <div class="line"></div>
+
+            <div class="media-options">
+                <a href="#" class="field facebook">
+                    <i class='bx bxl-facebook facebook-icon'></i>
+                    <span>Login with Facebook</span>
+                </a>
+            </div>
+
+            <div class="media-options">
+                <a href="#" class="field google">
+                    <img src="images/google.png" alt="" class="google-img">
+                    <span>Login with Google</span>
+                </a>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- JavaScript -->
+    <script src="./scripts/login.js"></script>
+</body>
+
+</html>
