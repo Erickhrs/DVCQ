eb711827e58eb68995d09cdfef1a771f8086f929
Erickhrs <erickhenriquee4444@gmail.com>
2024-10-01 12:05:26 -0300

    saving chages

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $wrong = total_user_cw($mysqli, $_SESSION['id'], '0'); // Total de erros
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./assets/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="./style/global.css">
     <link rel="stylesheet" href="./style/index.css">
     <title>DVC - QUESTÕES</title>
@@ -36,14 +37,30 @@ $wrong = total_user_cw($mysqli, $_SESSION['id'], '0'); // Total de erros
                     </a></li>
             </ul>
             <main id="home-menu-root">
-                <h1>Bem vindo de volta, <?php echo $_SESSION['name']?></h1>
-                <div>
+                <h1 id="welcome_user_title">Bem vindo de volta, <?php echo $_SESSION['name']?></h1>
+                <div id="general_st">
                     <h6>Questões Resolvidas: <?php echo total_questions_answered($mysqli, $_SESSION['id']); ?>
                     </h6>
                     <h6 id="correct">Questões Acertadas: <?php echo $correct; ?></h6>
                     <h6 id="wrong">Questões Erros: <?php echo $wrong; ?></h6>
                 </div>
 
+                <div class="container_ind">
+                    <h1>Simulados</h1>
+                    <div class="search-box">
+                        <input type="text" placeholder="Busque pelo nome">
+                        <button>
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                    <div class="simulado-card">
+                        <h2>(Amostra) Projeto Decorando a Lei Seca - 1ª Semana</h2>
+                        <p>155 questões</p>
+                        <div class="footer-card">
+                            <i class="fa fa-calendar"></i>
+                            <span>04/04/2023</span>
+                        </div>
+                    </div>
                 </div>
             </main>
         </nav>
