febe595b4ae4d123c23c05faa4a29223ba185033
Erickhrs <erickhenriquee4444@gmail.com>
2024-10-08 01:09:57 -0300

    saving

--- scripts/answer_validator.js
+++ scripts/answer_validator.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
+    // Inicializa as variáveis de contagem
+    var corrects = 0;
+    var wrongs = 0;
+
+    // Manipulador de envio de formulário
     $('.question').on('submit', function(event) {
         event.preventDefault(); // Evita o envio do formulário
 
         const $form = $(this);
-        const questionID = $form.find('.span_number_list').text().replace('#', '');
-        const selectedOption = $form.find('input[type="radio"]:checked');
+        const questionID = $form.find('.span_number_list').text().replace('#', ''); // Obtém o ID da questão
+        const selectedOption = $form.find('input[type="radio"]:checked'); // Verifica a opção selecionada
 
         if (selectedOption.length > 0) {
-            const alternative = selectedOption.val();
+            const alternative = selectedOption.val(); // Obtém o valor da alternativa selecionada
 
+            // Envia a requisição AJAX
             $.ajax({
                 url: './actions/validate_answer.php',
                 type: 'POST',
@@ -17,13 +23,21 @@ $(document).ready(function() {
                     alternative: alternative
                 },
                 success: function(response) {
-                    // Verifica a resposta do servidor ('correct' ou 'wrong')
+                    // Verifica a resposta do servidor
                     if (response.trim() === 'correct') {
-                        // Se correto, exibe o cmsg e esconde o wmsg
+                        // Incrementa a contagem de respostas corretas
+                        corrects += 1;
+                        console.log('Corretas: ' + corrects);
+
+                        // Exibe a mensagem de resposta correta
                         $('#cmsg_' + questionID).show();
                         $('#wmsg_' + questionID).hide();
                     } else if (response.trim() === 'wrong') {
-                        // Se errado, exibe o wmsg e esconde o cmsg
+                        // Incrementa a contagem de respostas erradas
+                        wrongs += 1;
+                        console.log('Erradas: ' + wrongs);
+
+                        // Exibe a mensagem de resposta errada
                         $('#wmsg_' + questionID).show();
                         $('#cmsg_' + questionID).hide();
                     }
